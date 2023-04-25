edb7d6d511c15fdacaefc55b7870bc9690fc016b
unknown <tommy@grandcircus.co>
2023-04-25 12:01:34 -0400

    Added Animal children

---
+++ Week 3 - Objects and Classes/AnimalAbstract/AnimalAbstract/Bush.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalAbstract
+{
+    internal class Bush : Animal
+    {
+        public Bush() : base(50)
+        {
+        }
+
+        public override void Eat(int amount, FoodType food)
+        {
+            if(food == FoodType.Light)
+            {
+                CaloriesEaten += amount;
+            }
+        }
+
+
+    }
+}
