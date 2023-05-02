fa3f47f6fc9e237bb56d3ba065421e8dacd55c5d
unknown <tommy@grandcircus.co>
2023-05-02 13:31:29 -0400

    Added TDD Samples

---
+++ Week 4 - Unit Tests and SQL/UnitTesting/UnitTesting/PasswordVerifier.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting
+{
+    internal class PasswordVerifier
+    {
+        public bool Verify(string password)
+        {
+            bool notNull = password != null;
+            //Not null check has to happen first, as the other conditions will throw an exception 
+            //if we are null
+            if (notNull)
+            {
+                bool longEnough = password.Length >= 8;
+                bool hasUpper = HasUppercase(password);
+                bool hasLower = HasLowercase(password);
+                if (longEnough && hasUpper && hasLower)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return false;
+        }
+
+        public bool HasUppercase(string password)
+        {
+            foreach(char letter in password)
+            {
+                bool b = char.IsUpper(letter);
+                if(b == true)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool HasLowercase(string password)
+        {
+            foreach (char letter in password)
+            {
+                bool b = char.IsLower(letter);
+                if (b == true)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Week 4 - Unit Tests and SQL/UnitTesting/UnitTesting/TestPassword.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTesting
+{
+    public class TestPassword
+    {
+        [Theory]
+        [InlineData("helloworld", false)]
+        [InlineData("short", false)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+        [InlineData("Helloworld", true)]
+        [InlineData("HELLOWORLD", false)]
+        public void CheckPassword(string password, bool expected)
+        {
+            //Arrange 
+            PasswordVerifier pv = new PasswordVerifier();
+
+            //Act 
+            bool actual = pv.Verify(password);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
