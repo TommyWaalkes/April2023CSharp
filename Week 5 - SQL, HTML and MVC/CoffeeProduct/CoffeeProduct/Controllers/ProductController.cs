12a75b2ae762f89916c64783e221b170b202a935
Josh <thejosh2021@gmail.com>
2023-05-12 15:43:49 -0400

    added product example

---
+++ Week 5 - SQL, HTML and MVC/CoffeeProduct/CoffeeProduct/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using CoffeeProduct.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeProduct.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private readonly ProductDbContext _context;
+        public ProductController(ProductDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> allProducts = _context.Products.ToList();
+            return View(allProducts);
+        }
+
+        public IActionResult Detail(int productId)
+        {
+            Product product = _context.Products.FirstOrDefault(p=> p.Id == productId);
+            return View(product);
+        }
+
+        // Filter Products example, not implemented
+        public IActionResult Filter(int minPrice)
+        {
+            List<Product> ExpensiveProducts = _context.Products.Where(p => p.Price > minPrice).ToList();
+            return View(ExpensiveProducts);
+        }
+    }
+}
---
+++ Week 5 - SQL, HTML and MVC/CoffeeProduct/CoffeeProduct/Models/ProductDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeProduct.Models
+{
+    public class ProductDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=ProdcutsDb;Trusted_Connection=True;TrustServerCertificate=True");
+            }
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
