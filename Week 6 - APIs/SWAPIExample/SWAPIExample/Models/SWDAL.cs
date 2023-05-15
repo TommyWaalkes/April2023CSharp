77f045f712d3538376ae854d0d8f34a806b88f67
TommyWaalkes <tommy@grandcircus.co>
2023-05-15 12:03:05 -0400

    Added SWAPI example

---
+++ Week 6 - APIs/SWAPIExample/SWAPIExample/Models/SWDAL.cs
@@ -0,0 +1,14 @@
+﻿using RestSharp;
+
+namespace SWAPIExample.Models
+{
+    public class SWDAL
+    {
+        public SWPerson GetPerson(int id)
+        {
+            RestClient client = new RestClient($"https://swapi.dev/api/people/{id}/");
+            RestRequest request = new RestRequest();
+            Task<SWPerson> response = client.GetAsync<SWPerson>(request);
+        }
+    }
+}
