5f7073689b2e7d4ec67b2121e4773fcf2dc96c13
TommyWaalkes <tommy@grandcircus.co>
2023-05-15 11:06:39 -0400

    Added MVC Pet example

--- Week 6 - APIs/AdoptionMVC/AdoptionMVC/Controllers/HomeController.cs
+++ Week 6 - APIs/AdoptionMVC/AdoptionMVC/Controllers/HomeController.cs
@@ -15,7 +15,42 @@ namespace AdoptionMVC.Controllers
 
         public IActionResult Index()
         {
+            List<Pet> pets = db.Pets.ToList();
+            //Use linq to filter out duplicate breeds 
+            List<Pet> filtered  = pets.DistinctBy(p => p.Breed).ToList();
+            return View(filtered);
+        }
+
+        public IActionResult Result(string breed)
+        {
+            List<Pet> pets = db.Pets.Where(p => p.Breed == breed).ToList();
+            return View(pets);
+        }
+
+        public IActionResult Adopt(int Id)
+        {
+            Pet p = db.Pets.Find(Id);
+            db.Pets.Remove(p);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        //For add we will do 2 actions: one to display the view, one to handle input from the form on the view 
+
+        //This action displays the view 
+        public IActionResult Add()
+        {
             return View();
+        }
+
+        //This action handles the input from the add pet form 
+        [HttpPost] //Post is a type of HTTP call, usually used for adding a new item to a database
+                   //I'm putting it here to tell .Net this action is used for adding to the DB and NOT for displaying a view
+        public IActionResult Add(Pet p)
+        {
+            db.Pets.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         public IActionResult Animal()
