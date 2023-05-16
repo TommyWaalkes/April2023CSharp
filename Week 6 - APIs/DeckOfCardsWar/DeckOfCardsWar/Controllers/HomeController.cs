a1a2bb7ed108499b4a3ec16962c5da92ffb15b9e
TommyWaalkes <tommy@grandcircus.co>
2023-05-16 09:52:42 -0400

    Added War Example

---
+++ Week 6 - APIs/DeckOfCardsWar/DeckOfCardsWar/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using DeckOfCardsWar.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DeckOfCardsWar.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        DeckDAL cardAPI = new DeckDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Deck d = cardAPI.GetDeck();
+            List<PlayerDeck> players = cardAPI.GetPlayerDecks(d.deck_id);
+
+
+            return View(players);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Week 6 - APIs/DeckOfCardsWar/DeckOfCardsWar/Models/DeckDAL.cs
@@ -0,0 +1,36 @@
+﻿using RestSharp;
+
+namespace DeckOfCardsWar.Models
+{
+    public class DeckDAL
+    {
+        public Deck GetDeck()
+        {
+            RestClient client = new RestClient($"https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+            RestRequest request = new RestRequest();
+            Task<Deck> response = client.GetAsync<Deck>(request);
+            Deck d = response.Result;
+
+            return d;
+        }
+
+        public List<PlayerDeck> GetPlayerDecks(string deckId)
+        {
+            RestClient client = new RestClient($"https://deckofcardsapi.com/api/deck/{deckId}/draw/?count=26");
+            RestRequest request = new RestRequest();
+            Task<PlayerDeck> response = client.GetAsync<PlayerDeck>(request);
+            PlayerDeck player1 = response.Result;
+
+            RestClient client2 = new RestClient($"https://deckofcardsapi.com/api/deck/{deckId}/draw/?count=26");
+            RestRequest request2 = new RestRequest();
+            Task<PlayerDeck> response2 = client2.GetAsync<PlayerDeck>(request2);
+            PlayerDeck player2 = response2.Result;
+
+            List<PlayerDeck> decks= new List<PlayerDeck>();
+            decks.Add(player1);
+            decks.Add(player2);
+
+            return decks;
+        }
+    }
+}
