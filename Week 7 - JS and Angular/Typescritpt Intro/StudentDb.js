a42c968915a672f071f5e0eb5f3f0b34fc1c4a08
TommyWaalkes <tommy@grandcircus.co>
2023-05-22 13:29:48 -0400

    added TS demo

--- Week 7 - JS and Angular/Typescritpt Intro/StudentDb.js
+++ Week 7 - JS and Angular/Typescritpt Intro/StudentDb.js
@@ -5,3 +5,17 @@ var s2 = { name: "Jane", favoriteFood: "Mac n Cheese", homeTown: "NYC" };
 students.push(s2);
 var s3 = { name: "Jeni", favoriteFood: "Salad", homeTown: "Raleigh NC" };
 students.push(s3);
+var list = document.getElementById("StudentList");
+console.log(list);
+for (var i = 0; i < students.length; i++) {
+    list.innerHTML += "<li>Name: ".concat(students[i].name, "</li> \n    <button onClick=\"StudentDetails(").concat(i, ")\"> Learn more about ").concat(students[i].name, " </button>");
+}
+function StudentDetails(index) {
+    var s = students[index];
+    var output = document.getElementById("Output");
+    //wipe out whats there already
+    output.innerHTML = "";
+    output.innerHTML += "<p> Name: ".concat(s.name, " </p>");
+    output.innerHTML += "<p> Hometown: ".concat(s.homeTown, " </p>");
+    output.innerHTML += "<p> Favorite Food: ".concat(s.favoriteFood, " </p>");
+}
--- Week 7 - JS and Angular/Typescritpt Intro/StudentDb.ts
+++ Week 7 - JS and Angular/Typescritpt Intro/StudentDb.ts
@@ -14,3 +14,22 @@ students.push(s2);
 
 let s3 : Student = {name:"Jeni", favoriteFood: "Salad", homeTown:"Raleigh NC"};
 students.push(s3);
+
+let list  = document.getElementById("StudentList"); 
+console.log(list);
+for(let i = 0; i < students.length; i++){
+    list.innerHTML += `<li>Name: ${students[i].name}</li> 
+    <button onClick="StudentDetails(${i})"> Learn more about ${students[i].name} </button>`;
+}
+
+function StudentDetails(index: number) : void{
+    let s: Student = students[index];
+
+    let output = document.getElementById("Output");
+    //wipe out whats there already
+    output.innerHTML= "";
+
+    output.innerHTML+= `<p> Name: ${s.name} </p>`
+    output.innerHTML+= `<p> Hometown: ${s.homeTown} </p>`
+    output.innerHTML+= `<p> Favorite Food: ${s.favoriteFood} </p>`
+}
