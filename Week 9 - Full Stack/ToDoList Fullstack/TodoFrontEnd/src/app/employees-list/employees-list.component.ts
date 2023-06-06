416bc0f9798510c16f1ed6868646f6dc30c4a3c0
TommyWaalkes <tommy@grandcircus.co>
2023-06-06 11:59:19 -0400

    Got Delete working with complex foreign key

--- Week 9 - Full Stack/ToDoList Fullstack/TodoFrontEnd/src/app/employees-list/employees-list.component.ts
+++ Week 9 - Full Stack/ToDoList Fullstack/TodoFrontEnd/src/app/employees-list/employees-list.component.ts
@@ -1,19 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../employee.service';
 import { Employee } from '../employee';
 import { ToDosService } from '../to-dos.service';
+import { ToDo } from '../toDo';
 
 @Component({
   selector: 'app-employees-list',
   templateUrl: './employees-list.component.html',
   styleUrls: ['./employees-list.component.css']
 })
-export class EmployeesListComponent {
+export class EmployeesListComponent implements OnInit {
   employees:Employee[] = [];
+  todos:ToDo[] = [];
+
+  //Runs once as the app loads
   constructor(private api:EmployeeService, private todoApi:ToDosService){
+  
+  }
+
+  //Runs any time we reload the component 
+  ngOnInit(): void {
     this.api.getAllEmployees().subscribe(
       (result) =>{
         this.employees = result;
+        this.todoApi.getAll().subscribe(
+          (result)=>{
+            this.todos = result;
+            this.fillOutTodos();
+          }
+        )
       }
     );
   }
@@ -21,6 +36,19 @@ export class EmployeesListComponent {
   //The backend wants to be passed the ID 
   //The front-end will have an easier with the index 
   deleteEmployee(id:number, index:number){
+    console.log(index);
+    let toDos: ToDo[] = this.employees[index].toDos; 
+
+    if(toDos.length >0 || this.todos !== undefined){
+      for(let i = 0; i < this.todos.length; i++){
+        this.todoApi.deleteTodo(toDos[i].id).subscribe(
+          ()=>{
+            toDos.splice(index, 1);
+          }
+        );
+      }
+    }
+
     this.api.deleteEmployee(id).subscribe(
       ()=>{
         this.employees.splice(index,1);
@@ -30,8 +58,8 @@ export class EmployeesListComponent {
 
   fillOutTodos(){
     for(let i =0; i < this.employees.length; i++){
-      let e  = this.employees[i];
-      e.toDos = this.todoApi.getToDosByEmployee(e.id);
+      //I pass in the full to do array that way we do not need to call the api every time the loop runs 
+      this.employees[i].toDos= this.todoApi.getToDosByEmployee( this.employees[i].id, this.todos);
     }
   }
 }
--- Week 9 - Full Stack/ToDoList Fullstack/TodoFrontEnd/src/app/to-dos.service.ts
+++ Week 9 - Full Stack/ToDoList Fullstack/TodoFrontEnd/src/app/to-dos.service.ts
@@ -8,26 +8,28 @@ import { ToDo } from './toDo';
 })
 export class ToDosService {
 
-  url:string ="https://localhost:7008/api/ToDo";
+  url:string ="https://localhost:7008/api/ToDo/";
   constructor(private http:HttpClient) { }
 
   getAll():Observable<ToDo[]>{
     return this.http.get<ToDo[]>(this.url);
   }
 
-  getToDosByEmployee(employeeId:number ):any{
-    this.getAll().subscribe(
-      (result)=>{
+  deleteTodo(id:number){
+    return this.http.delete<any>(this.url+id)
+  }
+
+  //Remember services can have methods beyond calling APIs
+  getToDosByEmployee(employeeId:number, todos:ToDo[] ):any{
+    
         let output:ToDo[] = []; 
-        for(let i = 0; i < result.length; i++){
-          if(result[i].assignedTo == employeeId){
-            output.push(result[i]);
+        for(let i = 0; i < todos.length; i++){
+          if(todos[i].assignedTo === employeeId){
+            output.push(todos[i]);
           }
         }
 
         return output;
       }
-    )
-  }
 
 }
